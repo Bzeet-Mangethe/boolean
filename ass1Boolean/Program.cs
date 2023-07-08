c1b73854117b4739d88c034c9e8664fe36b4403b
Bhekani Masinga <bhekani.masinga@nagarro.com>
2023-07-08 18:40:48 +0200

    first commit

---
+++ ass1Boolean/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass1Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a boolean (true or false): ");
+            string input = Console.ReadLine();
+            bool result;
+            if (bool.TryParse(input, out result))
+            {
+                Console.WriteLine("Parsed boolean: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+        }
+    }
+}
